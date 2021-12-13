6b1e02843e7c9f226a3102fafc92aa038db6c026
Marcelo Humerez Mars <mhumerez@tecnimotors.com>
2021-12-13 08:57:50 -0500

    Primer Push de backend

---
+++ backend2/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace backend2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        // GET: api/<DefaultController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Aplicacion corriendo ...";
+        }
+
+        
+    }
+}
---
+++ backend2/Persistence/Context/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using backend2.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend2.Persistence.Context
+{
+    public class AplicationDbContext: DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
